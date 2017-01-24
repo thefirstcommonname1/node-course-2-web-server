a8e4d1af51fb3ef025081e691371c8cde12e4033
Bishav Kharel <bishavkharel@Bishavs-MacBook-Pro.local>
2017-01-24 14:30:58 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(function (req, res, next) {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});// middle ware
+
+// app.use(function(req, res, next) {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', function () {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', function(text) {
+  return text.toUpperCase();
+});
+
+app.get('/', function(req, res) {
+  //res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs', {
+    pageTitle: "Home page",
+    welcomeMessage: "Wecome to Some website"
+  })
+});
+
+app.get('/about', function(req, res){
+  res.render('about.hbs',{
+    pageTitle: "About page",
+
+  });
+});
+
+app.get('/bad', function(req,res){
+  res.send({
+    errorMessage: "Unable to handle request"
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server is up on port 3000");
+});
