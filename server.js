16164325c192b96ae700639601a06974ee1e56f4
thefirstcommonname1 <kharelbishav4@gmail.com>
2017-01-24 15:47:13 -0500

    Added projects template

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get('/about', function(req, res){
   });
 });
 
+app.get('/projects', function(req, res){
+  res.render('projects.hbs', {
+    pageTitle: "Projects page"
+  })
+})
+
 app.get('/bad', function(req,res){
   res.send({
     errorMessage: "Unable to handle request"
